06586b096ac420754546675d39d32ea122cd2cb5
risto <risto.saar@gmail.com>
2019-11-14 19:12:35 +0900

    delegated transger

--- CrappToken/token_sale/contracts/CrappToken.sol
+++ CrappToken/token_sale/contracts/CrappToken.sol
@@ -11,8 +11,15 @@ contract CrappToken{
         uint256 _value
     );
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(uint256 _initalSupply) public {
         totalSupply = _initalSupply;
@@ -29,4 +36,26 @@ contract CrappToken{
 
        return true;
     }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+       allowance[msg.sender][_spender] = _value;
+
+       emit Approval(msg.sender,_spender,_value);
+
+       return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success){
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[_from][msg.sender]-=_value;
+        emit Transfer(_from,_to,_value);
+        return true;
+    }
+
+
 }
